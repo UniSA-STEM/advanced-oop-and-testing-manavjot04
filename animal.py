0777f64b9da1d8b34bae63964ce712998477d7ad
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-11-11 18:43:28 +1030

    Added the severity level and basic health record structure.

--- animal.py
+++ animal.py
@@ -7,6 +7,7 @@ Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 from enum import Enum, auto
+from datetime import date
 
 class DietType(Enum):
     HERBIVORE = auto()
@@ -20,3 +21,20 @@ class EnvironmentType(Enum):
     DESERT = auto()
     AVIARY = auto()
     REPTILE_HOUSE = auto()
+
+class SeverityLevel(Enum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+    CRITICAL = 4
+
+class HealthIssue:
+    def __init__(self, description, severity):
+        self.description = description
+        self.severity = severity
+        self.reported_on = date.today()
+        self.status = "OPEN"
+
+class HealthRecord:
+    def __init__(self):
+        self.issues = []
