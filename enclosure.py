192a3014a1d4e60b314095de4fd0053e7c2b8133
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-11-17 19:33:25 +1030

    Created the enclosure class with core attributes and moving on to work with adding the feaure to add and remove the animal.

--- enclosure.py
+++ enclosure.py
@@ -6,3 +6,14 @@ ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+
+from animal import EnvironmentType
+
+class Enclosure:
+    def __init__(self, name, species_allowed, environment, capacity):
+        self.name = name
+        self.species_allowed = species_allowed
+        self.environment = environment
+        self.capacity = capacity
+        self.cleanliness = 100
+        self.animals = []
