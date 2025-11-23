d8fa98468a59c2ca904132546922cfbac2423961
manavjot04 <dutmy005@mymail.unisa.edu.au>
2025-11-23 12:28:49 +1030

    Added my demo script and finalized the system for submission.

--- main.py
+++ main.py
@@ -6,3 +6,26 @@ ID: 110100110
 Username: bizvy001
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
+from animal import Mammal, Reptile, Bird, DietType, EnvironmentType, SeverityLevel
+from enclosure import Enclosure
+from staff import Zookeeper, Veterinarian
+
+def main():
+    lion_enclosure = Enclosure("Savannah Exhibit", "Lion", EnvironmentType.SAVANNAH, 3)
+    simba = Mammal("Simba", "Lion", 5, DietType.CARNIVORE, 8, EnvironmentType.SAVANNAH)
+    lion_enclosure.add_animal(simba)
+
+    keeper = Zookeeper("Ava")
+    keeper.assign_animal(simba)
+    keeper.assign_enclosure(lion_enclosure)
+
+    vet = Veterinarian("Dr. Leo")
+    vet.assign_animal(simba)
+
+    print(keeper.feed(simba))
+    print(vet.diagnose(simba, "Leg sprain", SeverityLevel.MEDIUM, "Rest"))
+    print(vet.discharge(simba))
+    print(lion_enclosure.status())
+
+if __name__ == "__main__":
+    main()
